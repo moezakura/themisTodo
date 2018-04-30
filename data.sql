65b476f6122954cb90305b98a500b68ea95ef666
Masaki Furukawa <c38p8d@gmail.com>
2018-05-01 02:26:19 +0900

    projectのdbを追加

--- data.sql
+++ data.sql
@@ -6,3 +6,11 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`uuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 COMMIT;
+
+CREATE TABLE IF NOT EXISTS `projects` (
+  `uuid` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(256) COLLATE utf8_bin NOT NULL,
+  `description` varchar(1024) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+COMMIT;
