9c9db5cd5bfbcc7947ac985b806e1ec57662ff86
Masaki Furukawa <c38p8d@gmail.com>
2018-05-01 05:12:55 +0900

    ProjectsとUserを紐付けるdbの追加

--- data.sql
+++ data.sql
@@ -7,10 +7,26 @@ CREATE TABLE IF NOT EXISTS `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 COMMIT;
 
+
 CREATE TABLE IF NOT EXISTS `projects` (
   `uuid` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(256) COLLATE utf8_bin NOT NULL,
   `description` varchar(1024) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`uuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+COMMIT;
+
+
+CREATE TABLE IF NOT EXISTS `users_in_projects` (
+  `user_id` int(11) NOT NULL,
+  `project_id` int(11) NOT NULL,
+  `enable` tinyint(1) NOT NULL,
+  `expiration` datetime DEFAULT NULL,
+  KEY `project_id` (`project_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+ALTER TABLE `users_in_projects`
+  ADD CONSTRAINT `users_in_projects_ibfk_2` FOREIGN KEY (`project_id`) REFERENCES `projects` (`uuid`),
+  ADD CONSTRAINT `users_in_projects_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `users` (`uuid`);
 COMMIT;
