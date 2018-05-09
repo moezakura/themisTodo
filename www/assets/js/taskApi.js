e717a76419877e93f0c2564f4e0f447b1eb601c9
Masaki Furukawa <c38p8d@gmail.com>
2018-05-09 19:41:47 +0900

    タスク移動を送信するまで実装

--- www/assets/js/taskApi.js
+++ www/assets/js/taskApi.js
@@ -1,13 +1,42 @@
 class TaskApi {
-    static GetTaskFromCreateDate(createDate) {
-        let params = {
-            "time": createDate
+    static NewTaskObject() {
+        return {
+            "status": -1,
+            "name": "",
+            "creator": -1,
+            "creatorName": "",
+            "id": -1,
+            "deadline": "",
+            "description": ""
         };
-        let esc = encodeURIComponent;
-        let query = Object.keys(params).map(k => esc(k) + '=' + esc(params[k])).join('&');
+    }
 
-        return fetch("/task?" + query, {
+    static stringToIntStatus(str) {
+        switch (str) {
+            case "todo":
+                return 0;
+            case "doing":
+                return 1;
+            case "pr":
+                return 2;
+            case "done":
+                return 3;
+        }
+    }
+
+    static GetTaskFromCreateDate(createDate) {
+        return fetch("/task/" + createDate, {
             method: 'GET',
+            credentials: "same-origin"
+        }).then(function (response) {
+            return response.json();
+        });
+    }
+
+    static Update(createDate, taskApi) {
+        return fetch("/task" + createDate + "/update", {
+            method: 'POST',
+            body: JSON.stringify(taskApi),
             credentials: "same-origin"
         }).then(function (response) {
             return response.json();
--- www/assets/js/taskBoard.js
+++ www/assets/js/taskBoard.js
@@ -6,9 +6,17 @@ var taskLists = [
 ];
 
 for (var i = 0; i < taskLists.length; i++) {
+    var statusId = i;
     Sortable.create(taskLists[i], {
-        group: {
-            name: "shares",
+        group: "shares",
+        onEnd: function (evt) {
+            let statusStr = evt.item.parentNode.dataset.status;
+            let targetCreateId = evt.item.dataset.id;
+            let task = TaskApi.NewTaskObject();
+
+            task.status = TaskApi.stringToIntStatus(statusStr);
+
+            TaskApi.Update(targetCreateId, task);
         },
         animation: 100
     });
