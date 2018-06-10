39adee79c10701509e564da9f24f1b2564db8af1
Masaki Furukawa <mox@g.mox.si>
2018-06-10 09:52:33 +0900

    コミット漏れ

---
+++ models/projectDeleteResultJson.go
@@ -0,0 +1,6 @@
+package models
+
+type ProjectDeleteResultJson struct {
+	Success bool   `json:"success"`
+	Message string `json:"message"`
+}
---
+++ www/assets/js/projectDelete.js
@@ -0,0 +1,54 @@
+var deleteProject = document.querySelector("#deleteProject"),
+    deleteProjectPopup = document.querySelector("#deleteProjectPopup"),
+    deleteProjectPopupCloseButton = document.querySelector("#deleteProjectPopup .close"),
+    deleteProjectNameInput = document.querySelector("#deleteProjectPopup input[type=text]"),
+    deleteProjectNameSubmitButton = document.querySelector("#deleteProjectPopup input[type=submit]"),
+    deleteProjectPopupError = deleteProjectPopup.querySelector(".error");
+
+deleteProject.addEventListener("click", deleteProjectPopupShow, true);
+deleteProjectPopupCloseButton.addEventListener("click", deleteProjectPopupClose, true);
+deleteProjectPopup.addEventListener("submit", deleteProjectPopupSend, true);
+deleteProjectNameInput.addEventListener("keyup", deleteProjectNameInputKeyUp, true);
+deleteProjectPopupError.addEventListener("click", deleteProjectPopupErrorClick, true);
+
+function deleteProjectPopupShow() {
+    deleteProjectPopup.style.display = "block";
+}
+
+function deleteProjectPopupClose() {
+    deleteProjectPopup.style.display = "none";
+}
+
+function deleteProjectPopupErrorClick() {
+    deleteProjectPopupErrorHide();
+}
+
+function deleteProjectPopupErrorHide() {
+    deleteProjectPopupError.style.display = "none";
+}
+
+function deleteProjectPopupErrorShow(text) {
+    deleteProjectPopupError.style.display = "block";
+    if (text !== undefined && text != null) {
+        deleteProjectPopupError.innerText = text;
+    }
+}
+
+function deleteProjectNameInputKeyUp() {
+    let typeText = deleteProjectNameInput.value;
+    deleteProjectNameSubmitButton.disabled = (typeText !== projectName);
+}
+
+function deleteProjectPopupSend(e) {
+    e.preventDefault();
+    fetch("/project/delete/" + projectId, {
+        method: 'POST',
+        body: "",
+        credentials: "same-origin"
+    }).then(function (response) {
+        return response.json();
+    }).then(function (json) {
+        if (json.success) location.href = "/home";
+        else deleteProjectPopupErrorShow(json.message);
+    });
+}
