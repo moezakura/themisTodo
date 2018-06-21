422a37bfb805a15d289bc4da1e320626818cf74c
URANO Ryoya <urano.works.mail@gmail.com>
2018-06-22 04:50:24 +0900

    taskIdからTaskを取得するAPIに対応

--- www/assets/js/taskApi.js
+++ www/assets/js/taskApi.js
@@ -33,6 +33,15 @@ export default class TaskApi {
         });
     }
 
+    static GetTaskFromTaskId(taskId) {
+        return fetch("/tasks/view/taskId/" + taskId, {
+            method: 'GET',
+            credentials: "same-origin"
+        }).then(function (response) {
+            return response.json();
+        });
+    }
+
     static Create(taskJson){
         return fetch("/tasks/create", {
             method: 'POST',
