cbddb3fae9a470a8cb36a7ce5828c0236e2cadb6
NISHI3 <ryonishi3@gmail.com>
2018-06-27 01:16:35 +0900

    querySelectorを使って#loginのときのみ読み込むようにした

--- www/assets/js/login.js
+++ www/assets/js/login.js
@@ -1,38 +1,40 @@
 import Vue from 'vue';
 
-new Vue({
-    delimiters: ['${', '}'],
-    el: '#login',
-    data: {
-        formError: null,
-        formUsername: '',
-        formPassword: '',
-        hideStyle: 'none'
-    },
-    methods: {
-        postLogin() {
-            let loginJson = {
-                "id": this.formUsername,
-                "password": this.formPassword
-            }
-            fetch("", {
-                method: 'POST',
-                body: JSON.stringify(loginJson),
-                credentials: "same-origin"
-            }).then((response) => {
-                return response.json()
-            }).then((json) => {
-                if(!json.success) {
-                    this.formError = json.message
-                    this.hideStyle = 'block'
-                } else {
-                    this.hideStyle = 'none'
-                    location.href = 'home'
-                }
-            })
+if(document.querySelector("#login")) {
+    new Vue({
+        delimiters: ['${', '}'],
+        el: '#login',
+        data: {
+            formError: null,
+            formUsername: '',
+            formPassword: '',
+            hideStyle: 'none'
         },
-        clickError() {
-            this.hideStyle = 'none'
-        }
-    },
-})
+        methods: {
+            postLogin() {
+                let loginJson = {
+                    "id": this.formUsername,
+                    "password": this.formPassword
+                }
+                fetch("", {
+                    method: 'POST',
+                    body: JSON.stringify(loginJson),
+                    credentials: "same-origin"
+                }).then((response) => {
+                    return response.json()
+                }).then((json) => {
+                    if (!json.success) {
+                        this.formError = json.message
+                        this.hideStyle = 'block'
+                    } else {
+                        this.hideStyle = 'none'
+                        location.href = 'home'
+                    }
+                })
+            },
+            clickError() {
+                this.hideStyle = 'none'
+            }
+        },
+    })
+}
