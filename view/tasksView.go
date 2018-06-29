b15786384fe9def52935effbcef5aaca955a07c0
NISHI3 <ryonishi3@gmail.com>
2018-06-29 13:50:46 +0900

    消すの忘れてしまった :pray:

--- view/tasksView.go
+++ view/tasksView.go
@@ -25,7 +25,3 @@ func (self TasksView) GetView(c *gin.Context, statusCode int, json *models.TaskG
 func (self TasksView) GetMy(c *gin.Context, statusCode int, json *models.TasksGetResultJson) {
 	c.JSON(statusCode, json)
 }
-
-func (self TasksView) GetProjects(c *gin.Context, statusCode int, json *models.ProjectGetResultJson) {
-	c.JSON(statusCode, json)
-}
