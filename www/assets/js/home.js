8c4dd0751b5f3fad8954d13835a76fac91110a0a
NISHI3 <ryonishi3@gmail.com>
2018-06-30 15:54:05 +0900

    homePageをVueに書き換えた(js)

--- www/assets/js/home.js
+++ www/assets/js/home.js
@@ -1,22 +1,52 @@
-import ProjectUtils from './projectUtils.js';
+import Vue from 'vue';
+import MyTaskList from './components/ListComponent.vue'
 
-class Home {
-    constructor() {
-        this.todoListElem = document.querySelector("#todoList");
-        this.doingListElem = document.querySelector("#doingList");
-        if (this.todoListElem === undefined || this.todoListElem == null) return;
-
-        let that = this;
-        todoList.forEach(function (value) {
-            let item = ProjectUtils.createTaskItem(value.createDate, value.name, value.taskId, value.assignName, value.assign, value.deadline, value.limitDate);
-            that.todoListElem.appendChild(item);
-        });
-
-        doingList.forEach(function (value) {
-            let item = ProjectUtils.createTaskItem(value.createDate, value.name, value.taskId, value.assignName, value.assign, value.deadline, value.limitDate);
-            that.doingListElem.appendChild(item);
-        });
-    }
+if(document.querySelector("#home")) {
+    new Vue({
+        delimiters: ['${', '}'],
+        el: '#home',
+        data: {
+            todoList: [],
+            doingList: [],
+            projects: [],
+        },
+        components: {
+            "parent": MyTaskList
+        },
+        created () {
+            this.getList('TODO').then(data => this.todoList = data)
+            this.getList('DOING').then(data => this.doingList = data)
+            this.getProject().then(data => this.projects = data)
+        },
+        methods : {
+            getList(status) {
+                return fetch(`/tasks/my?status=${status}`, {
+                    method: 'GET',
+                    credentials: 'same-origin'
+                }).then(res => {
+                    return res.json()
+                }).then(json => {
+                    if(!json.success) {
+                        console.log(json.message)
+                    } else {
+                        return json.task
+                    }
+                })
+            },
+            getProject() {
+                return fetch("/project/my", {
+                    method: 'GET',
+                    credentials: 'same-origin'
+                }).then(res => {
+                    return res.json()
+                }).then(json => {
+                    if(!json.success) {
+                        console.log(json.message)
+                    } else {
+                        return json.project
+                    }
+                })
+            }
+        },
+    })
 }
-
-new Home();
