302ed2e32abe084d85512997ab1cd57db2569fb4
Masaki Furukawa <mox@g.mox.si>
2018-11-08 10:54:28 +0900

    Password変更をvueに置き換えた

--- www/assets/js/accountConfig.js
+++ www/assets/js/accountConfig.js
@@ -1,145 +1,7 @@
-import AccountApi from "./api/AccountApi"
-import BackView from "./backView"
-
-class AccountConfig {
-    constructor() {
-        this.idChangeForm = document.querySelector("#idChange");
-        if (this.idChangeForm === undefined || this.idChangeForm == null) return;
-        return;
-
-        this.displayNameChangeForm = document.querySelector("#displayNameChange");
-        this.passwordChangeForm = document.querySelector("#passwordChange");
-        this.idChangeFormError = this.idChangeForm.querySelector(".error");
-        this.displayNameChangeFormError = this.displayNameChangeForm.querySelector(".error");
-        this.passwordChangeFormError = this.passwordChangeForm.querySelector(".error");
-        this.changeSuccess = document.querySelector(".success");
-        this.idChangeSubmitFake = this.idChangeForm.querySelector(".fas.fa-check");
-        this.idChangeSubmitButton = this.idChangeForm.querySelector("input[type=submit]");
-        this.displayNameChangeSubmitFake = this.displayNameChangeForm.querySelector(".fas.fa-check");
-        this.displayNameChangeSubmitButton = this.displayNameChangeForm.querySelector("input[type=submit]");
-        this.currentPasswordDialog = document.querySelector("#currentPasswordDialog");
-
-        this.backView = new BackView();
-        this.backView.addWithHideElem(this.currentPasswordDialog);
-
-        this.currentPasswordInput = this.currentPasswordDialog.querySelector("#currentPasswordInput");
-
-        let dialogList = document.querySelectorAll(".error, .success");
-        dialogList.forEach(function (value) {
-            value.addEventListener("click", function (e) {
-                this.style.display = "none";
-            }, true);
-        });
-
-        document.AccountConfig = this;
-        let that = this;
-
-        this.idChangeForm.addEventListener("submit", function (e) {
-            that.idChangeSubmit(e, this, that);
-        }, true);
-        this.displayNameChangeForm.addEventListener("submit", function (e) {
-            that.displayNameChangeSubmit(e, this, that);
-        }, true);
-        this.passwordChangeForm.addEventListener("submit", function (e) {
-            that.passwordChangeSubmit(e, this, that);
-        }, true);
-        this.idChangeSubmitFake.addEventListener("click", function () {
-            that.idChangeSubmitButton.click();
-        }, true);
-        this.displayNameChangeSubmitFake.addEventListener("click", function () {
-            that.displayNameChangeSubmitButton.click();
-        }, true);
-        this.currentPasswordDialog.addEventListener("submit", function (e) {
-            that.passwordChangeConfirmSubmit(e, this, that);
-        }, true);
-
-
-        var selectPassword = "";
-    }
-
-    idChangeSubmit(e, _this, that) {
-        e.preventDefault();
-        let targetForm = new FormData(_this);
-        let changeObj = AccountApi.NewAccountObject(accountUuid);
-        changeObj.name = targetForm.get("accountSettingsId");
-        AccountApi.Change(changeObj).then(function (json) {
-            if (!json.success) {
-                that.idChangeFormError.style.display = "block";
-                that.idChangeFormError.innerText = json.message;
-                that.changeSuccess.style.display = "none";
-            } else {
-                that.idChangeFormError.style.display = "none";
-                that.changeSuccess.style.display = "block";
-            }
-        })
-    }
-
-    displayNameChangeSubmit(e, _this, that) {
-        e.preventDefault();
-        let targetForm = new FormData(_this);
-        let changeObj = AccountApi.NewAccountObject(accountUuid);
-        changeObj.displayName = targetForm.get("accountSettingsDisplayName");
-        AccountApi.Change(changeObj).then(function (json) {
-            if (!json.success) {
-                that.displayNameChangeFormError.style.display = "block";
-                that.displayNameChangeFormError.innerText = json.message;
-                that.changeSuccess.style.display = "none";
-            } else {
-                that.displayNameChangeFormError.style.display = "none";
-                that.changeSuccess.style.display = "block";
-            }
-        })
-    }
-
-    passwordChangeSubmit(e, _this, that) {
-        e.preventDefault();
-        let targetForm = new FormData(_this);
-        let password = targetForm.get("accountSettingsPassword"),
-            passwordRe = targetForm.get("accountSettingsPasswordRe");
-
-        if (password !== passwordRe) {
-            that.passwordChangeFormError.style.display = "block";
-            that.passwordChangeFormError.innerText = "password is not match";
-            that.changeSuccess.style.display = "none";
-            return;
-        }
-        that.selectPassword = password;
-        that.passwordChangeFormError.style.display = "none";
-        that.changeSuccess.style.display = "none";
-
-        that.currentPasswordDialog.style.display = "block";
-        that.backView.show();
-
-        that.currentPasswordInput.focus();
-    }
-
-    passwordChangeConfirmSubmit(e, _this, that) {
-        e.preventDefault();
-        if (that.currentPasswordDialog.style.display === "block") {
-            let changeObj = AccountApi.NewAccountObject(accountUuid);
-            changeObj.password = that.selectPassword;
-            changeObj.currentPassword = that.currentPasswordInput.value;
-            that.currentPasswordInput.value = "";
-            AccountApi.Change(changeObj).then(function (json) {
-                that.selectPassword = "";
-                if (!json.success) {
-                    that.passwordChangeFormError.style.display = "block";
-                    that.passwordChangeFormError.innerText = json.message;
-                    that.changeSuccess.style.display = "none";
-                } else {
-                    that.passwordChangeFormError.style.display = "none";
-                    that.changeSuccess.style.display = "block";
-                }
-
-                that.backView.hide();
-            })
-        }
-    }
-}
-
 import Vue from 'vue';
 import IdSetting from "./components/AccountSettings/IdSetting.vue"
-import DisplayNameSetting from "./components/AccountSettings/DisplayNameSetting";
+import DisplayNameSetting from "./components/AccountSettings/DisplayNameSetting"
+import PasswordChange from "./components/AccountSettings/PasswordChange"
 
 if(document.querySelector("#accountSettings")) {
     new Vue({
@@ -149,7 +11,8 @@ if(document.querySelector("#accountSettings")) {
         },
         components: {
             IdSetting,
-            DisplayNameSetting
+            DisplayNameSetting,
+            PasswordChange
         },
         created () {
 
@@ -157,6 +20,3 @@ if(document.querySelector("#accountSettings")) {
         methods : { },
     })
 }
-
-
-new AccountConfig();
---
+++ www/assets/js/components/AccountSettings/PasswordChange.vue
@@ -0,0 +1,110 @@
+<template>
+    <div>
+        <form id="passwordChange" @submit.prevent="submitPasswordChange">
+            <div class="success v-shown" v-show="changeSuccess" @click="clearMessages">Success changed</div>
+            <div class="error v-shown" v-show="errorMessage.length > 0" @click="clearMessages">{{ errorMessage }}</div>
+            <label for="accountSettingsPassword">New Password</label>
+            <input type="password" id="accountSettingsPassword" name="accountSettingsPassword" v-model="newPassword">
+            <label for="accountSettingsPasswordRe">New Password</label>
+            <input type="password" id="accountSettingsPasswordRe" name="accountSettingsPasswordRe"
+                   v-model="newPasswordRe">
+            <input type="submit" value="CHANGE">
+        </form>
+
+        <transition name="current-password-dialog" appear>
+            <div v-show="newPasswordChange">
+                <form id="currentPasswordDialog" class="basicForm" @submit.prevent="submitPasswordConfirm">
+                    <div class="loading-circle" v-if="loading"></div>
+                    <div v-else>
+                        <p>Current password</p>
+                        <input type="password" id="currentPasswordInput" v-model="currentPassword">
+                        <input type="submit" value="CONFIRM">
+                    </div>
+                </form>
+                <div id="backViewLayer" class="backView" @click="hideCurrentPasswordDialog"></div>
+            </div>
+        </transition>
+    </div>
+</template>
+
+<script lang="ts">
+    import AccountApi from "../../api/AccountApi"
+    import Account from "../../model/Account"
+
+    export default {
+        name: "PasswordChange",
+        data: () => {
+            return {
+                newPassword: "",
+                newPasswordRe: "",
+                currentPassword: "",
+                newPasswordChange: false,
+                changeSuccess: false,
+                loading: false,
+                errorMessage: "",
+            }
+        },
+        methods: {
+            showCurrentPasswordDialog() {
+                this.loading = false
+                this.newPasswordChange = true
+            },
+            hideCurrentPasswordDialog() {
+                this.newPasswordChange = false
+            },
+            clearMessages() {
+                this.errorMessage = ''
+                this.changeSuccess = false
+            },
+            submitPasswordChange() {
+                this.clearMessages()
+
+                if (this.newPassword !== this.newPasswordRe) {
+                    this.errorMessage = 'password is not match'
+                } else {
+                    this.errorMessage = ''
+                    this.showCurrentPasswordDialog()
+                }
+            },
+            submitPasswordConfirm() {
+                let changeObj = new Account()
+                changeObj.password = this.newPassword
+                changeObj.currentPassword = this.currentPassword
+
+                this.clearMessages()
+                this.loading = true
+
+                AccountApi.Change(changeObj).then(json => {
+                    if (!json.success) {
+                        this.errorMessage = json.message
+                        this.changeSuccess = false
+                    } else {
+                        this.errorMessage = ""
+                        this.changeSuccess = true
+                    }
+                }).finally(() => {
+                    this.hideCurrentPasswordDialog()
+                })
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .current-password-dialog-enter-active,
+    .current-password-dialog-leave-active {
+        transition: opacity .3s ease;
+    }
+
+    .current-password-dialog-enter,
+    .current-password-dialog-leave-to {
+        opacity: 0;
+    }
+
+    .loading-circle{
+        position: relative;
+        top: 50%;
+        left: 50%;
+        transform:translate(-50%, -50%);
+    }
+</style>
