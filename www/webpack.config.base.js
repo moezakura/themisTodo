21e3f46c702e8e2dd1f45a7501de0ae535b4b567
Masaki Furukawa <mox@g.mox.si>
2018-11-26 16:21:01 +0900

    webpackの設定をbaseを使うようにした

---
+++ www/webpack.config.base.js
@@ -0,0 +1,116 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const {VueLoaderPlugin} = require('vue-loader');
+
+const path = require('path');
+const dist = path.resolve(__dirname, 'dist');
+
+module.exports = (isDev) => {
+    const enabledSourceMap = isDev;
+
+    return {
+        entry: './src/main.ts',
+        mode: isDev ? "development" : "production",
+        output: {
+            filename: '[name]_[hash].js',
+            path: dist,
+            publicPath: '/'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: [
+                        {
+                            loader: 'ts-loader',
+                            options: {
+                                appendTsSuffixTo: [/\.vue$/]
+                            }
+                        }
+                    ]
+                },
+                {
+                    test: /\.css/,
+                    use: [
+                        'style-loader',
+                        {loader: 'css-loader', options: {url: false}},
+                    ],
+                },
+                {
+                    test: /\.scss/,
+                    use: [
+                        {
+                            loader: 'style-loader',
+                            options: {
+                                hmr: true,
+                                singleton: true,
+                            }
+                        },
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                url: false,
+                                sourceMap: enabledSourceMap,
+                                minimize: !enabledSourceMap,
+                                importLoaders: 2
+                            },
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: enabledSourceMap,
+                            }
+                        }
+                    ],
+                },
+                {
+                    test: /\.html$/,
+                    loader: 'html-loader'
+                },
+                {
+                    test: /\.vue$/,
+                    loader: 'vue-loader'
+                },
+                {
+                    test: /\.js$/,
+                    loader: 'babel-loader?optional[]=runtime',
+                    options: {
+                        presets: [
+                            ['env', {'modules': false}]
+                        ]
+                    },
+                    exclude: /node_modules/
+                },
+            ]
+        },
+        resolve: {
+            extensions: ['*', '.js', '.vue', '.json', '.tsx', '.ts'],
+            alias: {
+                '@components': path.resolve(__dirname, './src/assets/components'),
+                '@scripts': path.resolve(__dirname, './src/assets/scripts'),
+            }
+        },
+        plugins: [
+            new webpack.NamedModulesPlugin(),
+            new VueLoaderPlugin(),
+            new HtmlWebpackPlugin({
+                template: './src/index.html',
+                inject: 'body'
+            }),
+            new CopyWebpackPlugin([
+                {
+                    from: path.resolve(__dirname, 'src/assets/fontawesome/web-fonts-with-css/webfonts/'),
+                    to: path.resolve(dist, './webfonts/'),
+                },
+                {
+                    from: path.resolve(__dirname, 'src/assets/images/'),
+                    to: path.resolve(dist, './assets/images/'),
+                }
+            ]),
+        ]
+    };
+};
+if (module.hot) {
+    module.hot.accept();
+}
--- www/webpack.config.dev.js
+++ www/webpack.config.dev.js
@@ -1,133 +1,25 @@
-const MODE = 'development';
-const enabledSourceMap = (MODE === 'development');
-
 const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const {VueLoaderPlugin} = require('vue-loader');
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.config.base.js');
 
-const path = require('path');
-const dist = path.resolve(__dirname, 'dist');
-
-module.exports = {
-    entry: './src/main.ts',
-    mode: "development",
-    output: {
-        filename: '[name].js',
-        path: dist,
-        publicPath: '/'
-    },
+module.exports = merge(baseConfig(true), {
     devServer: {
         port: 8652,
         historyApiFallback: true,
+        inline: true,
         proxy: {
             '/api': {
                 target: 'http://localhost:31204',
-                pathRewrite: {'^/api' : ''}
+                pathRewrite: {'^/api': ''}
             }
         }
     },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: [
-                    {
-                        loader: 'ts-loader',
-                        options: {
-                            appendTsSuffixTo: [/\.vue$/]
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.css/,
-                use: [
-                    'style-loader',
-                    {loader: 'css-loader', options: {url: false}},
-                ],
-            },
-            {
-                test: /\.scss/, // 対象となるファイルの拡張子
-                use: [
-                    {
-                        loader: 'style-loader',
-                        options: {
-                            hmr: true,
-                            singleton: true,
-                        }
-                    },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            // オプションでCSS内のurl()メソッドの取り込みを禁止する
-                            url: false,
-                            // ソースマップの利用有無
-                            sourceMap: enabledSourceMap,
-
-                            // 0 => no loaders (default);
-                            // 1 => postcss-loader;
-                            // 2 => postcss-loader, sass-loader
-                            importLoaders: 2
-                        },
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            // ソースマップの利用有無
-                            sourceMap: enabledSourceMap,
-                        }
-                    }
-                ],
-            },
-            {
-                test: /\.html$/,
-                loader: 'html-loader'
-            },
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader'
-            },
-            {
-                test: /\.js$/,
-                loader: 'babel-loader?optional[]=runtime',
-                options: {
-                    presets: [
-                        ['env', {'modules': false}]
-                    ]
-                },
-                exclude: /node_modules/
-            },
-        ]
-    },
     resolve: {
-        extensions: ['*', '.js', '.vue', '.json', '.tsx', '.ts'],
         alias: {
             vue: 'vue/dist/vue.js',
-            '@components': path.resolve(__dirname, './src/assets/components'),
-            '@scripts': path.resolve(__dirname, './src/assets/scripts'),
         }
     },
     plugins: [
-        new webpack.NamedModulesPlugin(),
         new webpack.HotModuleReplacementPlugin(),
-        new VueLoaderPlugin(),
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            inject: 'body'
-        }),
-        new CopyWebpackPlugin([
-            {
-                from: path.resolve(__dirname, 'src/assets/fontawesome/web-fonts-with-css/webfonts/'),
-                to: path.resolve(dist, './webfonts/'),
-            },
-            {
-                from: path.resolve(__dirname, 'src/assets/images/'),
-                to: path.resolve(dist, './assets/images/'),
-            }
-        ]),
-    ],
-};
-if (module.hot) {
-    module.hot.accept();
-}
+    ]
+});
