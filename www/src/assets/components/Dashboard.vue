7b7a993705a7c33fd4048d1287f6c017f694ca90
mox <mox+git@mox.si>
2019-12-09 15:57:39 +0900

    Dashboardに進行中のタスクを表示するようにした

--- www/src/assets/components/Dashboard.vue
+++ www/src/assets/components/Dashboard.vue
@@ -1,30 +1,38 @@
 <template>
     <div id="home">
         <div id="home-container">
-            <section>
+            <section class="home-left-section">
                 <div class="sectionTitle">Your Projects</div>
                 <ul class="joinProject">
                     <project-line v-for="project in projects" :key="project.uuid" :project="project"></project-line>
                 </ul>
             </section>
-            <section>
-                <div class="sectionTitle taskListTitle">My Todo Tasks</div>
-                <ul class="taskList" id="todoList">
-                    <li v-for="task in todoList">
-                        <task-line :key="task.createDate" :task="task"
-                                   :hideAssign="true" :full-deadline="true"></task-line>
-                    </li>
-                </ul>
-            </section>
-            <section>
-                <div class="sectionTitle taskListTitle">My Doing Tasks</div>
-                <ul class="taskList" id="doingList">
-                    <li v-for="task in doingList">
-                        <task-line :key="task.createDate" :task="task" :hideAssign="true"
-                                   :full-deadline="true"></task-line>
-                    </li>
-                </ul>
-            </section>
+            <div class="home-right-section">
+                <section class="">
+                    <div class="sectionTitle taskListTitle">Current Doing Task</div>
+                    <task-timer-user-dashboard></task-timer-user-dashboard>
+                </section>
+                <div class="home-todo-list">
+                    <section class="todo-list">
+                        <div class="sectionTitle taskListTitle">My Todo Tasks</div>
+                        <ul class="taskList" id="todoList">
+                            <li v-for="task in todoList">
+                                <task-line :key="task.createDate" :task="task"
+                                           :hideAssign="true" :full-deadline="true"></task-line>
+                            </li>
+                        </ul>
+                    </section>
+                    <section class="todo-list">
+                        <div class="sectionTitle taskListTitle">My Doing Tasks</div>
+                        <ul class="taskList" id="doingList">
+                            <li v-for="task in doingList">
+                                <task-line :key="task.createDate" :task="task" :hideAssign="true"
+                                           :full-deadline="true"></task-line>
+                            </li>
+                        </ul>
+                    </section>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -36,10 +44,12 @@
     import ProjectLine from "@components/Common/ProjectLine"
     import TaskLine from "@components/TaskBoard/TaskLine"
     import Task from "../scripts/model/api/task/Task"
+    import TaskTimerUserDashboard from "@components/TaskTimer/TaskTimerUserDashboard.vue";
 
     export default {
         name: "Dashboard",
         components: {
+            TaskTimerUserDashboard,
             ProjectLine,
             TaskLine
         },
@@ -102,6 +112,96 @@
     }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+    .sectionTitle {
+        font-size: 16px;
+        letter-spacing: 1.3px;
+        height: 35px;
+        line-height: 35px;
 
+        i {
+            color: white;
+            margin-left: 10px;
+            line-height: 35px;
+        }
+    }
+
+    .taskListTitle {
+        height: $buttonHeight;
+        line-height: $buttonHeight;
+        background-color: rgba(black, .3);
+
+        &::before {
+            width: 5px;
+            height: 100%;
+            display: inline-block;
+            vertical-align: top;
+            margin-right: 10px;
+            content: " ";
+            background-color: $accentColor;
+        }
+    }
+
+
+    #home {
+        height: calc(100% - 75px);
+        overflow-x: auto;
+    }
+
+    #home-container {
+        display: flex;
+        justify-content: flex-start;
+        margin: 0 15px;
+        min-width: 1100px;
+    }
+
+    .home-left-section, .home-right-section {
+        flex-grow: 1;
+        margin: 0 10px;
+    }
+
+    .home-left-section {
+        width: 40%;
+    }
+
+    .home-right-section {
+        width: 60%;
+
+        * {
+            justify-content: unset;
+            flex-grow: 0;
+        }
+
+    }
+
+    .home-todo-list {
+        display: flex;
+
+        .todo-list {
+            width: calc((100% - 10px) / 2);
+
+            &:first-child {
+                margin-right: 10px;
+            }
+        }
+
+        ul {
+            &.joinProject {
+                display: block;
+                list-style: none;
+                margin: 0 0 0 15px;
+                padding: 0;
+            }
+
+            &.taskList {
+                width: 100%;
+
+                li {
+                    overflow: hidden;
+                    text-overflow: ellipsis;
+                    white-space: nowrap;
+                }
+            }
+        }
+    }
 </style>
---
+++ www/src/assets/components/TaskTimer/TaskTimerUserDashboard.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        name: "TaskTimerUserDashboard"
+    }
+</script>
+
+<style scoped lang="scss">
+
+</style>
