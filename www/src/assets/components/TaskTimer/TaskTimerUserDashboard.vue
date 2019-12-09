f22d7fa358cd12f9886516434dd60b24a3851055
mox <mox+git@mox.si>
2019-12-09 16:05:15 +0900

    CurrentDoingTaskのなにもないときを作った

--- www/src/assets/components/TaskTimer/TaskTimerUserDashboard.vue
+++ www/src/assets/components/TaskTimer/TaskTimerUserDashboard.vue
@@ -1,15 +1,33 @@
 <template>
     <div>
-
+        <p class="no-tasks" v-if="timerTasks.length === 0">There is no Doing task.</p>
     </div>
 </template>
 
 <script lang="ts">
+    import Task from "@scripts/model/api/task/Task";
+
+    interface TaskTimerUserDashboardData {
+        timerTasks: Array<Task>
+    }
+
     export default {
-        name: "TaskTimerUserDashboard"
+        name: "TaskTimerUserDashboard",
+        data(): TaskTimerUserDashboardData {
+            return {
+                timerTasks: [],
+            }
+        }
     }
 </script>
 
 <style scoped lang="scss">
-
+    .no-tasks {
+        text-align: center;
+        margin: 20px 0;
+        font-weight: 900;
+        opacity: 0.4;
+        letter-spacing: 1.2px;
+        font-size: 16px;
+    }
 </style>
